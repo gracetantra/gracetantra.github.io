53926930d8b600332e1d7d7ee96d59e1901706c7
Grace Tantra <49855668+gracetantra@users.noreply.github.com>
2020-09-17 14:19:44 -0700

    Updates

---
+++ precache-manifest.79cdc4ea132e3782907b1d4494ac780f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77cf5f6fee62d714f273b907db1240c",
+    "url": "/gracetantra.github.io/index.html"
+  },
+  {
+    "revision": "c6e14feba07930323d05",
+    "url": "/gracetantra.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4d2c9c273e14143c1d8",
+    "url": "/gracetantra.github.io/static/js/2.76040fd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gracetantra.github.io/static/js/2.76040fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e14feba07930323d05",
+    "url": "/gracetantra.github.io/static/js/main.6e85f7f2.chunk.js"
+  },
+  {
+    "revision": "93b95512a465f15f1932",
+    "url": "/gracetantra.github.io/static/js/runtime-main.83088f71.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97835be812c1263ac7282156c641df2f.js"
+  "/gracetantra.github.io/precache-manifest.79cdc4ea132e3782907b1d4494ac780f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gracetantra.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
